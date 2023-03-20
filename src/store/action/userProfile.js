5692294b3b8306b3985f059d3fdedeba61ba2bfd
prashant.basnet@selu.edu <prashant.basnet@selu.edu>
2023-03-20 10:14:36 -0400

    more on message seen

--- src/store/action/userProfile.js
+++ src/store/action/userProfile.js
@@ -1,4 +1,4 @@
-import { GET_USER_PROFILE, GET_USER_FRIENDS, GET_USER_MESSAGES } from "./types"
+import { GET_USER_PROFILE, GET_USER_FRIENDS, GET_USER_MESSAGES, MY_NETWORK_RECENT_MESSAGES } from "./types"
 
 export const
     getUserProfile = (data) => {
@@ -18,4 +18,12 @@ export const
             type: GET_USER_MESSAGES,
             payload: data
         }
+    },
+    setMyNetworkRecentMessages = (data) => {
+        return (dispatch) => {
+            dispatch({
+                type: MY_NETWORK_RECENT_MESSAGES,
+                payload: data
+            })
+          }
     }
--- src/ui/pages/messages/chatList/index.js
+++ src/ui/pages/messages/chatList/index.js
@@ -19,62 +19,70 @@ import { SkeletonMessage } from "../../../../utils/components/SkeletonMessage"
 import { useDispatch, useSelector } from "react-redux"
 import { sendMessageTo } from "../../../../store/action/userActivity"
 import { last } from "ramda"
+import { messageSeen } from "../../../../utils"
 
-export const Communicators = ({ socket = {}, messages = [], connectionList = [], connectionListWithMessage = [] }) => {
-        const
-            [isOpen, setIsOpen] = useState(false),
-            { user } = useSelector((store) => store?.userProfile),
-            //  { loading, error, data } = useQuery(getFriends, { context: { server: MESSAGE_SERVICE_GQL } }),
-              dispatch = useDispatch(),
-            // { loading, error, data } = useQuery(getFriends, { context: { server: MESSAGE_SERVICE_GQL } }),
-            setUpChat = (friend) => {
-                dispatch(sendMessageTo((friend?.user)))
-                if (socket.current && last(messages) && !(last(messages)?.seen) && last(messages)?.senderId !== user._id) {
-                    socket.current.emit("messageSeen", last(messages)?._id)
-                }
-            },
-            handleMessagesList = (communicators = connectionListWithMessage || connectionList) => {
+export const Communicators = ({ socket = {}, messages = [], connectionList = [], connectionListWithMessage = [], messagingTo = {} }) => {
+    const
+        [isOpen, setIsOpen] = useState(false),
+        { user } = useSelector((store) => store?.userProfile),
+        { username } = useParams(),
+        { recentMessages } = useSelector((store) => store?.userProfile),
+        //  { loading, error, data } = useQuery(getFriends, { context: { server: MESSAGE_SERVICE_GQL } }),
+        dispatch = useDispatch(),
+        // { loading, error, data } = useQuery(getFriends, { context: { server: MESSAGE_SERVICE_GQL } }),
+        setUpChat = (friend) => {
+            dispatch(sendMessageTo((friend?.user)))
+        },
+        // connectionListWithMessage is merged list of connectionList and it's recent message
+        handleMessagesList = (communicators = recentMessages || (connectionListWithMessage || connectionList)) => {
 
-                if (!communicators) {
-                    return <div>No messages</div>
-                }
-                return communicators?.map((item, index) => {
-                    // const { id, avatar, name, username, message, time } = c
-                    return <Link to={`/messages/${item?.user?.username}`} key={index} onClick = { () => setUpChat(item)}>
-                        <MessageItem {...item.user} {...item.recentMessage}/>
-                    </Link>
-                })
+            if (!communicators) {
+                return <div>No messages</div>
             }
-        return (
-            <>
-                <IonCard className="chat-list">
-                    <IonCardContent className="chat-list__container">
-                        <div className="flex-column ">
-                            <IonButton mode="ios" onClick={() => setIsOpen(true)}>
-                                New Messages
-                            </IonButton>
-                            <IonButton mode="ios" color="light">
-                                Screener
-                            </IonButton>
-                        </div>
+            return communicators?.map((item, index) => {
+                // const { id, avatar, name, username, message, time } = c
+                // console.log("item", item)
+                return <Link to={`/messages/${item?.user?.username}`} key={index} onClick={() => setUpChat(item)}>
+                    <MessageItem {...item.user} {...item.recentMessage} />
+                </Link>
+            })
+        }
 
-                        <IonList className="chat-list__users">
-                            <IonListHeader>
-                                <h2>Direct Messages</h2>
-                            </IonListHeader>
-                            <IonSearchbar mode="ios"></IonSearchbar>
+    useEffect(() => {
+        if (username) {
+            messageSeen({ messagingTo, username, recentMessages })
+        }
+    }, [username])
+    return (
+        <>
+            <IonCard className="chat-list">
+                <IonCardContent className="chat-list__container">
+                    <div className="flex-column ">
+                        <IonButton mode="ios" onClick={() => setIsOpen(true)}>
+                            New Messages
+                        </IonButton>
+                        <IonButton mode="ios" color="light">
+                            Screener
+                        </IonButton>
+                    </div>
 
-                            <div className="chat-list__user-list">
-                                {/* {
+                    <IonList className="chat-list__users">
+                        <IonListHeader>
+                            <h2>Direct Messages</h2>
+                        </IonListHeader>
+                        <IonSearchbar mode="ios"></IonSearchbar>
+
+                        <div className="chat-list__user-list">
+                            {/* {
                                     loading
                                         ? <SkeletonMessage />
                                         : handleMessagesList(getUsers)
                                 } */}
-                                {handleMessagesList()}
-                            </div>
-                        </IonList>
-                    </IonCardContent>
-                </IonCard>
-            </>
-        )
-    }
+                            {handleMessagesList()}
+                        </div>
+                    </IonList>
+                </IonCardContent>
+            </IonCard>
+        </>
+    )
+}
