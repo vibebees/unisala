1a7a574a2b0e01e5ea70d0ea7a1a61d1b4998f70
prashant.basnet@selu.edu <prashant.basnet@selu.edu>
2023-05-20 21:42:42 -0400

    adding build details

---
+++ src/ui/pages/roadmap/index.js
@@ -0,0 +1,104 @@
+// import React, { useState } from "react"
+// import { IonContent, IonList, IonItem, IonLabel, IonIcon } from "@ionic/react"
+// import {
+//   checkmarkCircleOutline,
+//   chevronDownOutline,
+//   chevronUpOutline
+// } from "ionicons/icons"
+// import { motion } from "framer-motion"
+// const StudyAbroadRoadmap = () => {
+//   const roadmapSteps = [
+//     {
+//       title: "Research Programs and Universities",
+//       content:
+//         "Start researching universities and programs that align with your academic interests and career goals. Consider factors such as reputation, program offerings, location, and cost of living."
+//     },
+//     {
+//       title: "Prepare for Standardized Tests",
+//       content:
+//         "Identify the standardized tests required by the universities you're applying to, such as the SAT or ACT for general admissions and the TOEFL or IELTS for English language proficiency. Create a study plan and gather study materials."
+//     },
+//     {
+//       title: "Book SAT and TOEFL Exams",
+//       content:
+//         "Visit the official websites for the SAT and TOEFL exams to register and schedule your test dates. Be mindful of the registration deadlines and select a convenient test center."
+//     },
+//     {
+//       title: "Start Application Process",
+//       content:
+//         "Begin the application process for your chosen universities. Gather the necessary documents, such as your academic transcripts, personal statement, and letters of recommendation. Follow the application guidelines provided by each university."
+//     },
+//     {
+//       title: "Request Transcripts and Recommendation Letters",
+//       content:
+//         "Reach out to your previous educational institutions to request official transcripts. Approach teachers, professors, or mentors for recommendation letters. Provide them with ample time to prepare and submit the letters on your behalf."
+//     },
+//     {
+//       title: "Complete Financial Aid Applications",
+//       content:
+//         "If you require financial aid, research and complete the necessary financial aid applications, such as the Free Application for Federal Student Aid (FAFSA) or the CSS Profile. Submit the applications within the specified deadlines."
+//     },
+//     {
+//       title: "Submit Applications to Universities",
+//       content:
+//         "Carefully review each university's application requirements and submit your completed applications. Pay attention to any application fees and ensure that all required documents are included."
+//     },
+//     {
+//       title: "Receive I-20 Form",
+//       content:
+//         "After being accepted by a university, they will issue you an I-20 form (Certificate of Eligibility for Nonimmigrant Student Status). This document is needed for your visa application."
+//     },
+//     {
+//       title: "Fill Out DS-160 Form",
+//       content:
+//         "Complete the DS-160 form online, which is the Nonimmigrant Visa Application form required by the U.S. Department of State. Provide accurate and complete information about your personal, educational, and travel details."
+//     },
+//     {
+//       title: "Schedule Visa Interview",
+//       content:
+//         "Book an appointment for a visa interview at the U.S. Embassy or Consulate in your country. Be prepared to provide the required documents, including your DS-160 confirmation page, I-20 form, financial statements, and a valid passport."
+//     }
+//   ]
+// const [expandedStep, setExpandedStep] = useState(null)
+
+//   const toggleStep = (index) => {
+//     if (expandedStep === index) {
+//       setExpandedStep(null)
+//     } else {
+//       setExpandedStep(index)
+//     }
+//   }
+//   return (
+//     <IonContent>
+//       <IonList>
+//         {roadmapSteps.map((step, index) => (
+//           <React.Fragment key={index}>
+//             <IonItem button onClick={() => toggleStep(index)}>
+//               <IonIcon icon={checkmarkCircleOutline} slot="start" />
+//               <IonLabel>{step.title}</IonLabel>
+//               <IonIcon
+//                 icon={
+//                   expandedStep === index ? chevronUpOutline : chevronDownOutline
+//                 }
+//                 slot="end"
+//               />
+//             </IonItem>
+//             {expandedStep === index && (
+//               <motion.div
+//                 initial={{ opacity: 0, height: 0 }}
+//                 animate={{ opacity: 1, height: "auto" }}
+//                 exit={{ opacity: 0, height: 0 }}
+//                 transition={{ duration: 0.3 }}
+//               >
+//                 <IonItem lines="none">
+//                   <IonLabel>{step.content}</IonLabel>
+//                 </IonItem>
+//               </motion.div>
+//             )}
+//           </React.Fragment>
+//         ))}
+//       </IonList>
+//     </IonContent>
+//   )
+// }
+// export default StudyAbroadRoadmap
