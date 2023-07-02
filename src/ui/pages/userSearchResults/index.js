d64896a603967d0fa15bdf5e528ae8c87280939c
callback cat <76839908+chamanbravo@users.noreply.github.com>
2023-07-02 19:46:50 +0545

    no filter siderbar for user search

--- src/ui/pages/userSearchResults/index.js
+++ src/ui/pages/userSearchResults/index.js
@@ -1,45 +1,45 @@
 import {
-    IonCard,
-    IonCardContent,
-    IonGrid,
-    IonRow,
-    IonCol,
-    IonContent
+  IonCard,
+  IonCardContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonContent
 } from "@ionic/react"
 import SearchResults from "./SearchResults"
-import Filter from "./Filter"
 import { useParams } from "react-router-dom"
-import useWindowWidth from "../../../hooks/useWindowWidth"
+// import Filter from "./Filter"
+// import useWindowWidth from "../../../hooks/useWindowWidth"
 import useDocTitle from "../../../hooks/useDocTitile"
 
 function index() {
-    const { name: query } = useParams()
-    useDocTitle("Search ᛫ " + query)
-    const windowWidth = useWindowWidth()
+  const { name: query } = useParams()
+  useDocTitle("Search ᛫ " + query)
+  // const windowWidth = useWindowWidth()
 
-    return (
-        <IonContent>
-            <IonGrid className="max-width-container">
-                <IonCard>
-                    <IonCardContent>
-                        <h1>Search Result for {`"${query}"`}: </h1>
-                    </IonCardContent>
-                </IonCard>
+  return (
+    <IonContent>
+      <IonGrid className="max-width-container">
+        <IonCard>
+          <IonCardContent>
+            <h1>Search Result For Users {`"${query}"`}: </h1>
+          </IonCardContent>
+        </IonCard>
 
-                <IonRow>
-                    {windowWidth >= 768 && (
-                        <IonCol className="filter-col">
-                            <Filter />
-                        </IonCol>
-                    )}
+        <IonRow>
+          {/* {windowWidth >= 768 && (
+            <IonCol className="filter-col">
+              <Filter />
+            </IonCol>
+          )} */}
 
-                    <IonCol className="result-col">
-                        <SearchResults query={query} />
-                    </IonCol>
-                </IonRow>
-            </IonGrid>
-        </IonContent>
-    )
+          <IonCol className="result-col">
+            <SearchResults query={query} />
+          </IonCol>
+        </IonRow>
+      </IonGrid>
+    </IonContent>
+  )
 }
 
 export default index
