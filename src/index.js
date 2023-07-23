22b55bb00c8b5f341deab3f1a53de5a2cf2d2ffe
ankitdev10 <leoankit35@gmail.com>
2023-07-23 22:53:26 +0545

    chore: setup tailwind

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from "./App"
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration"
 import reportWebVitals from "./reportWebVitals"
 import "./index.css"
+import "./theme/tailwind.css"
 import { Provider } from "react-redux"
 import { store } from "./store/store"
 import {
