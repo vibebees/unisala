0ba62d259bea8666ada8e02a4a1e93604bd7f2ed
prashant.basnet@selu.edu <prashant.basnet@selu.edu>
2023-08-12 13:57:57 -0400

    adding roadmap

--- src/ui/pages/roadmap/index.js
+++ src/ui/pages/roadmap/index.js
@@ -1,15 +1,19 @@
 import React, { useState } from "react"
-import { IonContent, IonCard, IonCardContent, IonIcon } from "@ionic/react"
+import { IonContent, IonCard, IonCardContent, IonIcon, IonGrid, IonRow, IonCol } from "@ionic/react"
 import {
     checkmarkCircle,
     chevronDownOutline,
-    chevronUpOutline
+    chevronUpOutline,
+    removeCircleOutline,
+    addCircleOutline
 } from "ionicons/icons"
 import { motion, AnimatePresence } from "framer-motion"
 import { roadmapSteps } from "./roadmap"
+import "./styles.css"
+import { createAvatar } from "@dicebear/core"
+import { thumbs } from "@dicebear/collection"
 
 export const StudyAbroadRoadmap = () => {
-
     const [expandedStep, setExpandedStep] = useState(null)
 
     const toggleStep = (index) => {
@@ -20,41 +24,80 @@ export const StudyAbroadRoadmap = () => {
         }
     }
 
+    const getColorForStep = (index, totalSteps) => {
+        const percentage = (index / (totalSteps - 1))
+        return `rgb(${255 * (1 - percentage)}, ${255 * percentage}, 0)`
+    }
+
     return (
-        <IonContent>
-            <div className="roadmap-container">
-                {roadmapSteps.map((step, index) => (
-                    <motion.div key={index}>
-                        <div className={`roadmap-step ${expandedStep === index ? "expanded" : ""}`} onClick={() => toggleStep(index)}>
-                            <div className="step-icon-container">
-                                <IonIcon icon={checkmarkCircle} className="step-icon" />
-                            </div>
-                            <div className="step-content">
-                                <div className="step-title">{step.title}</div>
-                                <AnimatePresence>
-                                    {expandedStep === index && (
-                                        <motion.div
-                                            initial={{ opacity: 0, height: 0 }}
-                                            animate={{ opacity: 1, height: "auto" }}
-                                            exit={{ opacity: 0, height: 0 }}
-                                            transition={{ duration: 0.3 }}
-                                        >
-                                            <IonCard>
-                                                <IonCardContent>
-                                                    {step.content}
-                                                </IonCardContent>
-                                            </IonCard>
-                                        </motion.div>
-                                    )}
-                                </AnimatePresence>
-                            </div>
-                            {index !== roadmapSteps.length - 1 && (
-                                <div className={`step-line ${expandedStep === index ? "expanded" : ""}`} />
-                            )}
+        <IonContent className="roadmap-background1">
+            <IonGrid style={{ maxWidth: "700px", margin: "auto" }}>
+                <IonRow>
+                    <IonCol>
+                        <img src="https://icsblog.s3.ap-south-1.amazonaws.com/blog/wp-content/uploads/2023/05/10171257/study-abroad-usa.jpg" alt="Study Abroad Roadmap" className="roadmap-banner" />
+                        <h2 className="roadmap-heading">Your Study In USA Roadmap</h2>
+                        <p className="roadmap-description">Embarking on the journey of studying abroad is an adventure of a lifetime. Like any great journey, having a roadmap ensures you do not miss a single step. Dive into each phase of your adventure below.</p>
+
+                    </IonCol>
+
+                </IonRow>
+                <IonRow>
+                    <IonCol>
+                        <div className="roadmap-container">
+                            {roadmapSteps.map((step, index) => (
+                                <motion.div key={index} className="roadmap-motion-div">
+                                    <div className={`roadmap-step`} onClick={() => toggleStep(index)}>
+
+                                        <div className="step-icon-container">
+                                            <img src={step.image} alt={`Step ${index + 1}`} className="step-image" style={{ width: "100px" }} />
+                                        </div>
+
+                                        <div className="step-content">
+                                            <span className="step-number">{index + 1}</span>
+                                            <IonIcon icon={step.icon} className="step-icon" />
+                                            <div className="step-title">{step.title}</div>
+                                            <IonIcon icon={expandedStep === index ? chevronUpOutline : chevronDownOutline} className="toggle-icon" />
+                                        </div>
+                                    </div>
+
+                                    <AnimatePresence>
+                                        {expandedStep === index && (
+                                            <motion.div
+                                                initial={{ opacity: 0, height: 0 }}
+                                                animate={{ opacity: 1, height: "auto" }}
+                                                exit={{ opacity: 0, height: 0 }}
+                                                transition={{ duration: 0.3 }}
+                                                className="expandable-content"
+                                            >
+
+                                                <IonCard className="step-card">
+                                                    <IonCardContent>
+                                                        <ul className="content-list">
+                                                            {step.content.map((o, i) => (
+                                                                <li key={i} className="content-list-item">
+                                                                    {o}
+                                                                </li>
+                                                            ))}
+                                                        </ul>
+                                                    </IonCardContent>
+                                                </IonCard>
+
+                                            </motion.div>
+                                        )}
+                                    </AnimatePresence>
+                                </motion.div>
+                            ))}
                         </div>
-                    </motion.div>
-                ))}
-            </div>
+                    </IonCol>
+                </IonRow>
+                <IonRow>
+                    <IonCol>
+                        <img src="https://i.ibb.co/BPjHZby/prashantbasnet94-happineess-and-exitement-in-Nepalese-student-f-c6dc665d-6e44-45c1-a807-0ac59a0fac93.png" alt="Study Abroad Roadmap" className="roadmap-banner" />
+
+                    </IonCol>
+
+                </IonRow>
+            </IonGrid>
         </IonContent>
     )
 }
--- src/ui/pages/roadmap/roadmap.js
+++ src/ui/pages/roadmap/roadmap.js
@@ -1,52 +1,123 @@
+import {
+    schoolOutline, bookOutline, calendarOutline, clipboardOutline,
+    documentOutline, walletOutline, sendOutline, fileTrayStackedOutline,
+    bookmarkOutline, chatboxEllipsesOutline
+} from "ionicons/icons"
+
 export const roadmapSteps = [
     {
         title: "Research Programs and Universities",
-        content:
-            "Start researching universities and programs that align with your academic interests and career goals. Consider factors such as reputation, program offerings, location, and cost of living."
+        icon: schoolOutline,
+        content: [
+            "Begin by understanding your academic and career aspirations.",
+            "Evaluate universities based on their reputation, programs, location, tuition fees, and living costs.",
+            "Dive deeper by exploring:",
+            "How your preferred major fares in terms of visa applications.",
+            "Opportunities available after graduation.",
+            "Insights into tuition fees and scholarship opportunities.",
+            "Feedback on specific departments and professors.",
+            "Understanding the application process and fees."
+        ],
+        image: "https://i.ibb.co/VxWnJ5m/prashantbasnet94-Researching-Programs-and-Universities-392496a7-c562-43a4-8666-98a2e5164058.png"
     },
     {
         title: "Prepare for Standardized Tests",
-        content:
-            "Identify the standardized tests required by the universities you're applying to, such as the SAT or ACT for general admissions and the TOEFL or IELTS for English language proficiency. Create a study plan and gather study materials."
+        icon: bookOutline,
+        content: [
+            "Determine which standardized tests are needed for your prospective universities.",
+            "For general admissions, familiarize yourself with the SAT or ACT.",
+            "For English proficiency, prepare for tests like TOEFL or IELTS.",
+            "Create a study plan early on and gather essential materials.",
+            "Consider joining a test preparation course or seeking tutoring if needed."
+        ],
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/SAT_logo_%282017%29.svg/1200px-SAT_logo_%282017%29.svg.png"
     },
     {
         title: "Book SAT and TOEFL Exams",
-        content:
-            "Visit the official websites for the SAT and TOEFL exams to register and schedule your test dates. Be mindful of the registration deadlines and select a convenient test center."
+        icon: calendarOutline,
+        content: [
+            "Visit the official websites of SAT and TOEFL to register.",
+            "Choose test dates that allow ample time for preparation and re-takes if necessary.",
+            "Mind the registration deadlines and select a convenient test center.",
+            "Secure your spot early, especially during peak application seasons."
+        ],
+        image: "https://i.ibb.co/McW6Gqr/prashantbasnet94-Prepare-for-Standardized-Tests-SAT-ACT-TOEFL-I-c7b2fd1f-ecf9-4667-b53c-50fe8f07eee5.png"
     },
     {
         title: "Start Application Process",
-        content:
-            "Begin the application process for your chosen universities. Gather the necessary documents, such as your academic transcripts, personal statement, and letters of recommendation. Follow the application guidelines provided by each university."
+        icon: clipboardOutline,
+        content: [
+            "Research the specific application procedures for each university.",
+            "Gather crucial documents such as academic transcripts, personal statements, and letters of recommendation.",
+            "Stay organized, maintain a checklist, and adhere to application guidelines.",
+            "Take note of application fees and ensure to make the necessary payments."
+        ],
+        image: "https://i.ibb.co/kmvtxg3/prashantbasnet94-applying-to-universtiies-in-usa-b491c11f-c222-4405-89bc-b2dd38bec11d.png"
     },
     {
         title: "Request Transcripts and Recommendation Letters",
-        content:
-            "Reach out to your previous educational institutions to request official transcripts. Approach teachers, professors, or mentors for recommendation letters. Provide them with ample time to prepare and submit the letters on your behalf."
+        icon: documentOutline,
+        content: [
+            "Reach out to your former educational institutions for official transcripts.",
+            "Approach individuals who can vouch for your academic prowess or character for recommendation letters.",
+            "Provide referees with context, and enough time to craft compelling letters.",
+            "Follow up politely to ensure all your documents are sent in a timely manner."
+        ],
+        image: "https://i.ibb.co/VxWnJ5m/prashantbasnet94-Researching-Programs-and-Universities-392496a7-c562-43a4-8666-98a2e5164058.png"
     },
     {
         title: "Complete Financial Aid Applications",
-        content:
-            "If you require financial aid, research and complete the necessary financial aid applications, such as the Free Application for Federal Student Aid (FAFSA) or the CSS Profile. Submit the applications within the specified deadlines."
+        icon: walletOutline,
+        content: [
+            "Research potential financial aid opportunities.",
+            "For U.S. schools, consider applying through FAFSA or CSS Profile.",
+            "Submit all required documents and ensure you meet application deadlines.",
+            "Explore scholarship opportunities specific to your field of study or background."
+        ],
+        image: "https://i.ibb.co/PCCYYp1/prashantbasnet94-fill-out-ds160-form-and-books-visa-interview-d-636b0b57-4bfb-4c37-9231-a41bd3782e59.png"
     },
     {
         title: "Submit Applications to Universities",
-        content:
-            "Carefully review each university's application requirements and submit your completed applications. Pay attention to any application fees and ensure that all required documents are included."
+        icon: sendOutline,
+        content: [
+            "Review application requirements for each university meticulously.",
+            "Ensure that your application is complete, and all documents are attached.",
+            "Pay any associated application fees.",
+            "Double-check all entries before submission, ensuring accuracy and completeness."
+        ],
+        image: "https://i.ibb.co/VxWnJ5m/prashantbasnet94-Researching-Programs-and-Universities-392496a7-c562-43a4-8666-98a2e5164058.png"
     },
     {
         title: "Receive I-20 Form",
-        content:
-            "After being accepted by a university, they will issue you an I-20 form (Certificate of Eligibility for Nonimmigrant Student Status). This document is needed for your visa application."
+        icon: fileTrayStackedOutline,
+        content: [
+            "Upon university acceptance, await the I-20 form.",
+            "This document, issued by U.S. schools, is essential for your visa application.",
+            "Ensure all details on the I-20 form match your passport and other documents.",
+            "Safekeep this form; you'll need it throughout your study duration in the U.S."
+        ],
+        image: "https://i.ibb.co/yBBwyL0/prashantbasnet94-i20-acceptance-university-accpetance-letter-96573aa4-85c7-4876-a558-88fc95951bbf.png"
     },
     {
         title: "Fill Out DS-160 Form",
-        content:
-            "Complete the DS-160 form online, which is the Nonimmigrant Visa Application form required by the U.S. Department of State. Provide accurate and complete information about your personal, educational, and travel details."
+        icon: bookmarkOutline,
+        content: [
+            "Navigate to the U.S. Department of State's website to access the DS-160 form.",
+            "Complete the online nonimmigrant visa application form.",
+            "Provide accurate details regarding your personal, educational, and travel information.",
+            "After submission, print the confirmation page for your records."
+        ],
+        image: "https://i.ibb.co/PCCYYp1/prashantbasnet94-fill-out-ds160-form-and-books-visa-interview-d-636b0b57-4bfb-4c37-9231-a41bd3782e59.png"
     },
     {
         title: "Schedule Visa Interview",
-        content:
-            "Book an appointment for a visa interview at the U.S. Embassy or Consulate in your country. Be prepared to provide the required documents, including your DS-160 confirmation page, I-20 form, financial statements, and a valid passport."
+        icon: chatboxEllipsesOutline,
+        content: [
+            "Book a visa interview appointment at the nearest U.S. Embassy or Consulate.",
+            "Prepare essential documents: DS-160 confirmation, I-20 form, financial proofs, and a valid passport.",
+            "Anticipate common interview questions and prepare your responses.",
+            "On the day of the interview, arrive early, and dress professionally."
+        ],
+        image: "https://i.ibb.co/0F1z7hc/prashantbasnet94-go-for-usa-visa-interview-d05b911b-d23f-48d5-8ea8-f120138abf37.png"
     }
 ]
