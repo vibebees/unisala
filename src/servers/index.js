df99cfdb9c2b1116ef63e3c947242816bea2409b
prashant.basnet@selu.edu <prashant.basnet@selu.edu>
2023-08-16 00:21:13 -0400

    Update index.js

--- src/servers/index.js
+++ src/servers/index.js
@@ -1,41 +1,34 @@
+const config = require("./config")
+module.exports = (() => {
+  if (config.NODE_ENV === "DEVELOPMENT") {
+    return {
+      messagingServiceAddress: "http://localhost:2222",
+      universityServiceAddress: "http://localhost:9999",
+      messageSocketAddress: "ws://localhost:2224",
+      userServiceAddress: "http://localhost:4444",
+      callSocketAddress: "ws://localhost:4445"
+    }
+  }
+  if (config.NODE_ENV === "TEST") {
+    return {
+      messagingServiceAddress: "http://test.unisala.com/msg",
+      universityServiceAddress: "http://test.unisala.com/uni",
+      messageSocketAddress: "ws://test.unisala.com/msg",
+      userServiceAddress: "http://test.unisala.com/user",
+      callSocketAddress: "ws://localhost:4445"
+    }
+  }
 
-// eslint-disable-next-line no-process-env
-const NODE_ENV = process.env.NODE_ENV || "TEST"
-let config = {}
-
-switch (NODE_ENV) {
-
-    case "DEVELOPMENT":
-        config = {
-            messagingServiceAddress: "http://localhost:2222",
-            universityServiceAddress: "http://localhost:9999",
-            messageSocketAddress: "ws://localhost:2224",
-            userServiceAddress: "http://localhost:4444",
-            callSocketAddress: "ws://localhost:4445"
-        }
-        break
-    case "TEST":
-        config = {
-            messagingServiceAddress: "http://test.unisala.com/msg",
-            universityServiceAddress: "http://test.unisala.com/uni",
-            messageSocketAddress: "ws://test.unisala.com/msg",
-            userServiceAddress: "http://test.unisala.com/user",
-            callSocketAddress: "ws://localhost:4445"
-        }
-        break
-    case "PRODUCTION":
-        config = {
-            messagingServiceAddress: "http://unisala.com/msg",
-            universityServiceAddress: "http://unisala.com/uni",
-            messageSocketAddress: "ws://unisala.com/msg",
-            userServiceAddress: "http://unisala.com/user",
-            callSocketAddress: "ws://localhost:4445"
-        }
-        break
-    default:
-        config = {
-            base: "https://unisala.com"
-        }
-}
-
-module.exports = config
+  if (config.NODE_ENV === "PRODUCTION") {
+    return {
+      messagingServiceAddress: "http://unisala.com/msg",
+      universityServiceAddress: "http://unisala.com/uni",
+      messageSocketAddress: "ws://unisala.com/msg",
+      userServiceAddress: "http://unisala.com/user",
+      callSocketAddress: "ws://localhost:4445"
+    }
+  }
+  return {
+    base: "https://unisala.com"
+  }
+})()
