24a7866bf9012722b0fe82e9171c10f97325100d
immmdipu <dipu.chaurasiya@unisala.com>
2023-09-19 12:45:36 +0545

    user can now view post without login

--- src/utils/lib/protectedRoute.js
+++ src/utils/lib/protectedRoute.js
@@ -1,9 +1,21 @@
 import { useSelector } from "react-redux"
-import { Redirect } from "react-router-dom"
+import { Redirect, useLocation } from "react-router-dom"
 
 function ProtectedRoute({ children }) {
   const { loggedIn } = useSelector((state) => state?.userProfile)
-  return loggedIn ? children : <Redirect to="/home" />
+  const location = useLocation()
+  // eslint-disable-next-line prefer-destructuring
+  const id = location.pathname.split("/")[2]
+
+  if (!loggedIn && location.pathname === `/thread/${id}`) {
+    return children
+  }
+
+  if (!loggedIn) {
+    return <Redirect to="/" />
+  }
+
+  return children
 }
 
 export default ProtectedRoute
