954f6c3b3aedb12aa7de76f7363fc822b127ed52
immmdipu <dipu.chaurasiya@unisala.com>
2023-11-25 12:43:19 +0545

    Add ReactGA event tracking to ButtonTrack.js

--- src/features/analytics/ButtonTrack.js
+++ src/features/analytics/ButtonTrack.js
@@ -1,3 +1,11 @@
+import ReactGA from "react-ga4"
 export const ButtonTrack = (data) => {
-  console.log(data)
+  ReactGA.event({
+    category: "your category",
+    action: "your action",
+    label: "your label", // optional
+    value: 99, // optional, must be a number
+    nonInteraction: true, // optional, true/false
+    transport: "xhr" //
+  })
 }
