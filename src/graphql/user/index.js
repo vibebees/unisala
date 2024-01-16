72921feb792c62d1b735d84b5689364acb912757
immmdipu <dipu.chaurasiya@unisala.com>
2024-01-16 18:26:55 +0545

    Comment out unused rating fields and add a new line in AddComment query

--- src/graphql/user/index.js
+++ src/graphql/user/index.js
@@ -122,11 +122,11 @@ export const AddComment = gql`
           _id
           postText
           postCommentsCount
-          admissionAndApplicationRating
-          financialAidAndScholarshipRating
-          academicProgramsAndDepartmentRating
-          studentLifeAndServiceRating
-          careerAndAlumniResourceRating
+          # admissionAndApplicationRating
+          # financialAidAndScholarshipRating
+          # academicProgramsAndDepartmentRating
+          # studentLifeAndServiceRating
+          # careerAndAlumniResourceRating
           postImage
           date
           upVoted
@@ -469,6 +469,7 @@ export const AddComment = gql`
           _id
           images
           postText
+
           date
           upVoteCount
           postCommentsCount
