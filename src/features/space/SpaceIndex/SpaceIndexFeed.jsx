a0ad8953ffb1a9b1ae9256458f53280e45a82950
prashant.basnet@selu.edu <prashant.basnet@selu.edu>
2024-01-16 19:33:18 -0500

    added events card

--- src/features/space/SpaceIndex/SpaceIndexFeed.jsx
+++ src/features/space/SpaceIndex/SpaceIndexFeed.jsx
@@ -1,11 +1,14 @@
 import React from "react"
 import Thread from "../../../component/thread"
 import { IonContent } from "@ionic/react"
+import {EventCard} from "component/events"
 
 const SpaceIndexFeed = ({ posts }) => {
   return (
-    Array.isArray(posts) &&
-    posts.map((thread) => <Thread key={thread._id} thread={thread} />)
+    <>
+  { Array.isArray(posts) &&
+    posts.map((thread) => <Thread key={thread._id} thread={thread} />)}
+    </>
   )
 }
 
