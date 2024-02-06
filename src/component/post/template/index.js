9cb4fa0b718d31f8fec9971fbd472050123bac8f
prashant.basnet@selu.edu <prashant.basnet@selu.edu>
2024-02-05 19:55:47 -0500

    fixing metaData issue

--- src/component/post/template/index.js
+++ src/component/post/template/index.js
@@ -5,13 +5,23 @@ import { IonCard, IonTitle } from "@ionic/react"
 import { useEffect, useState } from "react"
 import axios from "axios"
 import { userServer } from "servers/endpoints"
-import { useHistory } from "react-router"
+import {useHistory} from "react-router"
+
+const usePathName = (index) => {
+  let currPath = window.location.pathname.split("/").filter((str) => str !== "")
+  const selectedSegment = currPath[index] || ""
+
+  return selectedSegment
+}
+
 export const CreateAPostCard = ({ allProps }) => {
   const { user } = useSelector((state) => state.userProfile)
   const { setCreateAPostPopUp } = allProps
   const [meta, setMeta] = useState({})
   const history = useHistory()
   const params = new URLSearchParams(window.location.href.search)
+  const pathname = usePathName(0) || "home"
+
   useEffect(() => {
     const fn = async () => {
       const res = await axios.get(userServer + "/getMetadataTags", {
@@ -19,8 +29,12 @@ export const CreateAPostCard = ({ allProps }) => {
           authorization: localStorage.getItem("accessToken")
         }
       })
-      setMeta(res.data?.data)
+      const metaData = res.data?.data || []
+      const getCurrentPageMetaData = metaData[pathname] || {}
+      const {addAPost} = getCurrentPageMetaData || {}
+      setMeta(addAPost)
     }
+
     fn()
   }, [])
 
